9c327160c50e5983f2c8caf9570b492467064af6
wonjae Lee <wonjaelee99@gmail.com>
2021-05-07 15:19:59 +0900

    test

--- src/pages/navi.js
+++ src/pages/navi.js
@@ -3,62 +3,36 @@ import {Container, Nav, NavItem} from "reactstrap";
 import "./css/navi.css"
 import Scrollchor from 'react-scrollchor';
 import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
-
-const Example = () => {
-    return (
-        <h1>Hi!!!</h1>
-    )
-}
-
-const Bye = () => {
-    return (
-        <h1>Bye!!</h1>
-    )
-}
+import About from './about.js'
+import Project from './project.js';
+import Work from './work.js';
+import Home from './home.js'
 
 class Navi extends React.Component {
 
   render() {
     return (
       <Container id = "Navi">
-        <Nav id = "NavItemList">
         <Router>
-            <Link to= "/example">Sample</Link>
-            <Link to = "/bye">bye</Link>
+            {/* <Link to= "/Home">Home</Link> */}
+            <Link to = "/About">About</Link>
+            <Link to = "/Work">Work</Link>
+            <Link to = "/Project">Project</Link>
             <Switch>
-            <Route path="/example">
-                <Example />
+            {/* <Route path="/Home">
+                <Home />
+            </Route> */}
+            <Route path="/About">
+                <About />
             </Route>
-            <Route path="/bye">
-                <Bye />
+            <Route path="/Work">
+                <Work />
+            </Route>
+            <Route path="/Project">
+                <Project />
             </Route>
             </Switch>
         </Router>
-          <NavItem>
-            <Scrollchor to = "#Home" className = "Nav-link">Home</Scrollchor>
-          </NavItem>
-          <NavItem>
-          <Scrollchor to = "#About" className = "Nav-link">About</Scrollchor>
-          </NavItem>
-          <NavItem>
-          <Scrollchor to = "#Work" className = "Nav-link">Work</Scrollchor>
-          </NavItem>
-          <NavItem>
-          <Scrollchor to = "Projects" className = "Nav-link">Projects</Scrollchor>
-          </NavItem>
-          {/* <NavItem>
-          <Scrollchor to = "#Skills" className = "Nav-link">Skills</Scrollchor>
-          </NavItem>
-          <NavItem>
-          <Scrollchor to = "#Contact" className = "Contact">Contact</Scrollchor>
-          </NavItem> */}
-          <NavItem>
-              <a href= "https://www.instagram.com/alarmcl0k/" target="_blank" rel="noreferrer">Contact Me</a>
-          </NavItem>
-          <NavItem>
-              <a href= "https://github.com/leewjae" target="_blank" rel="noreferrer">GitHub</a>
-          </NavItem>
-        </Nav>
       </Container>
     )
   }
